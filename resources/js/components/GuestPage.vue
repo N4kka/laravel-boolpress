51531e7bb81204ec6b689ecf415e9ceb191232f8
N4kka <sarlinicola7@gmail.com>
2022-07-05 17:07:58 +0200

    vue component created and linked to the vue app, vue app set as Home Page for non-registered users

---
+++ resources/js/components/GuestPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h2>Geust Home Page</h2>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'GuestPage',
+}
+</script>
+
+<style>
+
+</style>
--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h2>This is the Vue App</h2>
+    <GuestPage/>
   </div>
 </template>
 
 <script>
+import GuestPage from "../components/GuestPage.vue";
+
 export default {
   name: "app",
+  components: {
+    GuestPage,
+  },
 };
 </script>
 
