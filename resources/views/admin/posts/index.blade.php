39721511cd39f2a0a57ecd0d4b3d25241aaeb5fe
N4kka <sarlinicola7@gmail.com>
2022-07-06 14:24:05 +0200

    index page implemented, posts card created and filled with database data, after then looped and printed on screen

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,1 +1,20 @@
 @extends('layouts.dashboard')
+
+@section('content')
+    <h1>Hey, these are your posts:</h1>
+
+    <div class="row row-cols-4">
+        @foreach ($post as $post_item)
+            {{-- single post looped for each element in the db posts table --}}
+            <div class="card m-4" style="width: 18rem;">
+                {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                <div class="card-body">
+                    <h5 class="card-title">{{ $post_item->title }}</h5>
+                    <a href="#" class="btn btn-primary">Read the full post</a>
+                </div>
+            </div>
+            {{-- /single post looped for each element in the db posts table --}}
+        @endforeach
+    </div>
+    </div>
+@endsection
