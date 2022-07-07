d589e486e4ddf18cabe94b8ff13815d7e08e8792
N4kka <sarlinicola7@gmail.com>
2022-07-07 19:00:18 +0200

    migration, seeder and model added for user_info table, seeded it with some details and then I printed them in admin's dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts/dashboard')
 
 @section('content')
-    <h1>Ciao {{$user->name}}, sono l'Admin Home Page</h1>
+    <h1>Ciao {{ $user->name }}, sono l'Admin Home Page</h1>
+    <h2>Phone Number: +39{{ $user->userInfo->phone_number }}</h2>
+    <h2>Address: {{ $user->userInfo->address }}</h2>
+    <h2>Age: {{ $user->userInfo->age }} years old</h2>
 @endsection
